8d6a4e0ffe0ad6e02f94b646d8c783c8692765fb
COTEKieran <kieranblowes10@gmail.com>
2021-06-22 15:25:19 +0100

    Initial Commit

---
+++ GroundGuide/GroundGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroundGuideApp.swift
+//  GroundGuide
+//
+//  Created by Kieran Blowes on 22/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct GroundGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
